b8fda73ba72a952c0f7d236590004583573a6c7c
Jörg Polakowski <oltbaba@gmail.com>
2016-06-21 08:09:41 +0200

    initial import of EMS test suite

---
+++ EMS_Testsuite/helpers/AppiumiOSTestServer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace EMS_Testsuite
+{
+	public static class AppiumiOSTestServer
+	{
+		public static string Server1 { get { return "http://127.0.0.1:4723/wd/hub"; } }
+	}
+}
---
+++ EMS_Testsuite/iOSLoginTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.iOS;
+using NUnit.Framework;
+
+namespace AppiumTests
+{
+	[TestFixture]
+	public class iOSLoginTest
+	{
+		private AppiumDriver<IOSElement> driver;
+
+		[SetUp]
+		public void beforeAll()
+		{
+			// create the required capabilities
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.SetCapability("appium-version", "1.5.3");
+
+			capabilities.SetCapability("browserName", "iPad Air");
+			Platform macPlatform = new Platform(PlatformType.Mac);
+			capabilities.SetCapability("platform", macPlatform.ToString());
+			capabilities.SetCapability("platformName", "iOS");
+			capabilities.SetCapability("platformVersion", "9.3");
+			capabilities.SetCapability("deviceName", "iPad Air");
+
+			capabilities.SetCapability("keepKeyChains", true);
+			capabilities.SetCapability("noReset", true);
+			capabilities.SetCapability("launchTimeout", 120000); // 2min launch time out
+
+			capabilities.SetCapability("app", "/Users/macbuildserver/Development/001_TFS_build_agent/xcode_tfs_agent/_work/1/output/iphonesimulator/Debug/build.sym/Debug-iphonesimulator/EMS.app");
+
+			// selenium grid url
+			Uri serverUri = new Uri("http://10.127.141.71:4433/wd/hub");
+
+			Console.WriteLine("Server Uri: " + serverUri.ToString());
+
+			try
+			{
+				driver = new IOSDriver<IOSElement>(serverUri, capabilities);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception: " + e.ToString());
+			}
+		}
+
+		[TearDown]
+		public void afterAll()
+		{
+			Console.WriteLine("After All");
+
+			if (driver != null)
+			{
+				driver.Quit();
+			}
+			//if (!Env.isSauce())
+			//{
+			//	AppiumServers.StopLocalService();
+			//}
+		}
+
+		[Test]
+		public void LoginTest()
+		{
+			Console.WriteLine("iOS Login Test");
+
+			driver.FindElement(By.XPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATextField[1]")).SetImmediateValue("peliu3p1");
+			driver.FindElement(By.XPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIASecureTextField[1]")).SetImmediateValue("hSoRz3wo");
+			driver.FindElement(By.XPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAButton[1]")).Click();
+		}
+	}
+
+	public class MainTest
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Main...");
+		}
+	}
+}
