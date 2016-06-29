3a28c5354efe68743de9fae80c60b8f0e2441cb5
Jörg Polakowski <oltbaba@gmail.com>
2016-06-29 12:06:01 +0200

    added appium capabilities documentation

--- EMS_Testsuite/iOSLoginTest.cs
+++ EMS_Testsuite/iOSLoginTest.cs
@@ -15,24 +15,54 @@ namespace AppiumTests
 		[SetUp]
 		public void beforeAll()
 		{
-			// create the required capabilities
+			// Create the required capabilities for connecting with the Appium Selenium test runner
 			DesiredCapabilities capabilities = new DesiredCapabilities();
+
+			// Extensive list of Appium capability flags can be found here: https://github.com/appium/appium/blob/master/docs/en/writing-running-appium/caps.md
+
+			// Specify the Appium Version, which you are requesting from the Selenium runner
 			capabilities.SetCapability("appium-version", "1.5.3");
 
+			// iOS Specific settings for iOS Simulator --------------------------------------------------------------------
+
+			// The name of the browser, which is used to configure the appium runner with the Selenium grid
 			capabilities.SetCapability("browserName", "iPad Air");
+
+			// Set platform to "Mac" in order to 
 			Platform macPlatform = new Platform(PlatformType.Mac);
 			capabilities.SetCapability("platform", macPlatform.ToString());
+
+			// Which mobile OS platform to use, e.g. "iOS"
 			capabilities.SetCapability("platformName", "iOS");
+
+			// Mobile OS version (The requested version must be registered with the Selenium Grid)
 			capabilities.SetCapability("platformVersion", "9.3");
+
+			// The kind of mobile device or emulator to use (The device name must be registered with the Selenium Grid)
 			capabilities.SetCapability("deviceName", "iPad Air");
 
+			// (Simulator-only) Whether to keep keychains (Library/Keychains) when appium session is started/finished
 			capabilities.SetCapability("keepKeyChains", true);
+
+			// Don't reset app state before this session. (default: false)
 			capabilities.SetCapability("noReset", true);
+
+			// Amount of time in ms to wait for instruments before assuming it hung and failing the session
 			capabilities.SetCapability("launchTimeout", 120000); // 2min launch time out
 
-			capabilities.SetCapability("app", "/Users/macbuildserver/Public/001_xcode_share/development/EMS_share/Debug/EMS.ipa");
+			// ------------------------------------------------------------------------------------------------------------
 
-			// selenium grid url
+			// Dynamic environment identifier, which is used by the Xcode build agent 
+			// Possible values: "development", "production"
+			String tfsServerEnvironment = "development";
+
+			// Possbible values: "Debug", "Release"
+			String appConfiguration = "Debug";
+
+			// Tell the Appium runner where to find the IPA, which should be loaded into the iOS simulator when the tests are run.
+			capabilities.SetCapability("app", "/Users/macbuildserver/Public/001_xcode_share/" + tfsServerEnvironment + "/EMS_share/" + appConfiguration + "/EMS.ipa");
+
+			// Selenium grid url
 			Uri serverUri = new Uri("http://10.127.141.71:4433/wd/hub");
 
 			Console.WriteLine("Server Uri: " + serverUri.ToString());
@@ -56,10 +86,6 @@ namespace AppiumTests
 			{
 				driver.Quit();
 			}
-			//if (!Env.isSauce())
-			//{
-			//	AppiumServers.StopLocalService();
-			//}
 		}
 
 		[Test]
@@ -67,8 +93,8 @@ namespace AppiumTests
 		{
 			Console.WriteLine("iOS Login Test");
 
-			driver.FindElement(By.XPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATextField[1]")).SetImmediateValue("peliu3p1");
-			driver.FindElement(By.XPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIASecureTextField[1]")).SetImmediateValue("hSoRz3wo");
+			driver.FindElement(By.XPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATextField[1]")).SetImmediateValue("xxxx-user-xxxx");
+			driver.FindElement(By.XPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIASecureTextField[1]")).SetImmediateValue("xxxx-pass-xxxx");
 			driver.FindElement(By.XPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAButton[1]")).Click();
 		}
 	}
